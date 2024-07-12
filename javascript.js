f65e8dae20c2bd6eb0889e235a223e911b60996c
hanzsdev <149425914+hanzsdev@users.noreply.github.comm>
2024-07-12 16:40:46 +0100

    Add async checkWeather function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-console.log('hello')
+const apiKey = "4145e20d54efe66b564e9351fbbd9af0";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=paris";
+
+async function checkWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    const data = await response.json();
+
+    console.log(data.wind.speed);
+};
+
+checkWeather();
